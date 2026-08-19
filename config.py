657bc04ff72e4365510247ee1eef58b5fd3d86c2
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-19 15:18:23 +0630

    Update OWNER_IDS to include an additional ID
    Added a new owner ID to the OWNER_IDS list.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-OWNER_IDS = [7974865879]  # သင်၏ Telegram User ID
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_IDS = [7974865879, 7869852655]  # ထည့်သွင်းပေးထားသော Owner IDs များ
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///nexus.db")
 SPAWN_THRESHOLD = 15
 PORT = int(os.environ.get("PORT", 8080))
