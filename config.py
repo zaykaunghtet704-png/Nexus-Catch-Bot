c876f5edd474dd5e0e5bfa15591cc7433e1bd400
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-19 21:56:26 +0630

    Update SPAWN_THRESHOLD from 15 to 85

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_IDS = [7974865879, 7869852655]  # ထည့်သွင်းပေးထားသော Owner IDs များ
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///nexus.db")
-SPAWN_THRESHOLD = 15
+SPAWN_THRESHOLD = 85
 PORT = int(os.environ.get("PORT", 8080))
