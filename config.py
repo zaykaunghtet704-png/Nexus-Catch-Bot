aa09f69e5d50f96ad1b05539798f7042fd92957b
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-20 11:48:49 +0630

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///bot_database.db")
 PORT = int(os.getenv("PORT", 8080))
 
-# Super Owner Telegram IDs
-OWNER_IDS = [7974865879]
-
-DEFAULT_SPAWN_THRESHOLD = 80
+OWNER_IDS = [7974865879] # သင့် Telegram ID ထည့်ပါ
+DEFAULT_SPAWN_THRESHOLD = 85
