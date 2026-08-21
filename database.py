7e049b81bbaeafe1ba6faf0add26ebe321748f68
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-21 10:16:01 +0630

    Implement is_owner_or_sudo method in DatabaseManager
    Added a method to check if a user is an owner or a sudo user.

--- database.py
+++ database.py
@@ -1,20 +1,29 @@
+import time
+
 class DatabaseManager:
     def __init__(self):
         self.users = {}
         self.group_counts = {}
+        self.cards_db = {}
+        self.sudo_users = set()
 
     def get_user(self, uid: int, name: str = "User"):
         if uid not in self.users:
             self.users[uid] = {
                 "name": name,
                 "coins": 5000,
-                "cards": [],
+                "cards": [],       # [{id, card_key, print_num, mint, frame, dye, font, level}]
+                "inventory": {"frames": ["Neon", "Gold"], "dyes": ["#FF0055", "#00FFFF"], "fonts": ["Gothic", "Comic"]},
                 "hmode": "ALL",
-                "level": 1,
                 "is_banned": False,
                 "last_claim": 0,
-                "last_nclaim": 0
+                "last_nclaim": 0,
+                "rank_points": 0,
+                "guild": None
             }
         return self.users[uid]
+
+    def is_owner_or_sudo(self, uid: int, owner_ids: list):
+        return (uid in owner_ids) or (uid in self.sudo_users)
 
 db = DatabaseManager()
