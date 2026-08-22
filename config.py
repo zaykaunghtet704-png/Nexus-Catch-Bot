ded7f77f4054a47f8a4cc00176c5d2ee2d986a9a
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-22 11:27:10 +0630

    Modify bot configuration with new owner and links
    Updated OWNER_ID and added OWNER_USERNAME, CHANNEL_LINK, and GROUP_LINK.

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+OWNER_ID = int(os.getenv("OWNER_ID", "7974865879"))
+OWNER_USERNAME = "May_Be_786"
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "-100xxxxxxxxxx"))
+CHANNEL_LINK = "https://t.me/+E6BxfAj0gaI2Y2Zl"
+GROUP_LINK = "https://t.me/+00J7JktW8bJlZTY1"
