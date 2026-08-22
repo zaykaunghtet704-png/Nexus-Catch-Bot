ea258fd36ed868f5513bfc19bea2cff5723278b6
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-22 11:39:51 +0630

    Update LOG_CHANNEL_ID and maintain GROUP_LINK

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 OWNER_ID = int(os.getenv("OWNER_ID", "7974865879"))
 OWNER_USERNAME = "May_Be_786"
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "-100xxxxxxxxxx"))
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "-1003593049397"))
 CHANNEL_LINK = "https://t.me/+E6BxfAj0gaI2Y2Zl"
 GROUP_LINK = "https://t.me/+00J7JktW8bJlZTY1"
