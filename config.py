c16e2e384015fb3526e31c6e1d2f130703c063b5
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-22 14:50:47 +0630

    Refactor config to use hardcoded values and add TIER_PROBS
    Removed environment variable usage for OWNER_ID and OWNER_USERNAME, and added GROUP_LINK and TIER_PROBS dictionary.

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-OWNER_ID = int(os.getenv("OWNER_ID", "7974865879"))
-OWNER_USERNAME = "May_Be_786"
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "-1003593049397"))
-CHANNEL_LINK = "https://t.me/+E6BxfAj0gaI2Y2Zl"
+OWNER_ID = 7974865879
+OWNER_USERNAME = "@May_Be_786"
 GROUP_LINK = "https://t.me/+00J7JktW8bJlZTY1"
+CHANNEL_LINK = "https://t.me/+E6BxfAj0gaI2Y2Zl"
+
+# 13 Tiers & Probabilities
+TIER_PROBS = {
+    1: 40.0, 2: 25.0, 3: 15.0, 4: 8.0, 5: 5.0, 6: 3.0, 
+    7: 2.0, 8: 1.0, 9: 0.5, 10: 0.3, 11: 0.1, 12: 0.05, 13: 0.05
+}
