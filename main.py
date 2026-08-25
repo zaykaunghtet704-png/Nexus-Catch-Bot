3bbacedae2ba1aa6dab0a5e08c38ab0dd2134364
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-25 18:04:27 +0630

    Add catch router to the dispatcher

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import asyncio
 import logging
 
@@ -8,6 +9,7 @@ from database import init_db
 
 from handlers.basic import router as basic_router
 from handlers.cards import router as cards_router
+from handlers.catch import router as catch_router
 from handlers.daily import router as daily_router
 from handlers.pack import router as pack_router
 from handlers.owner import router as owner_router
@@ -34,6 +36,7 @@ async def main():
 
     dp.include_router(basic_router)
     dp.include_router(cards_router)
+    dp.include_router(catch_router)
     dp.include_router(daily_router)
     dp.include_router(pack_router)
     dp.include_router(owner_router)
