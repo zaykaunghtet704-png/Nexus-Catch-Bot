16fa1fa0a28f754c31eb003e3918174bfed7b280
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-26 19:48:38 +0630

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,16 @@ import os
 
 from dotenv import load_dotenv
 
+
 load_dotenv()
+
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 
-OWNER_ID = int(
-    os.getenv("OWNER_ID", "0").strip()
-)
+OWNER_ID_RAW = os.getenv("OWNER_ID", "0").strip()
+
+
+try:
+    OWNER_ID = int(OWNER_ID_RAW)
+except ValueError:
+    OWNER_ID = 0
