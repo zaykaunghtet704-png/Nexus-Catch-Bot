6d1c13262124961e03cebb21b9514c4df4e84aad
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-08-26 19:08:28 +0630

    Add configuration for bot token and admin IDs

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+ADMIN_IDS = {
+    int(x.strip())
+    for x in os.getenv("ADMIN_IDS", "").split(",")
+    if x.strip().isdigit()
+}
