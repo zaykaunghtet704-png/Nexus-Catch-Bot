4f080bd27586439d93365f6b03815228f9a160f1
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-09-22 20:15:04 +0630

    Change MongoDB URI and database name
    Updated MongoDB connection string and database name.

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
 import os
 from pymongo import MongoClient
 
-MONGO_URI = os.getenv("MONGO_URI")
+# MongoDB လိပ်စာကို ဤနေရာတွင် တိုက်ရိုက်ထည့်ပါ (အပေါ်က Username/Password ထည့်ထားပြီးသား Link အပြည့်အစုံ)
+MONGO_URI = "mongodb+srv://3Maybe:Nsnenfnddn283_283@cluster0.vdys92x.mongodb.net/?appName=Cluster0"
 
 if not MONGO_URI:
-    raise ValueError("MONGO_URI environment variable ကို ရှာမတွေ့ပါ။")
+    raise ValueError("MONGO_URI ကို ထည့်သွင်းရန် လိုအပ်ပါသည်။")
 
+# MongoDB client ချိတ်ဆက်ခြင်း
 client = MongoClient(MONGO_URI)
-db = client["waifu_bot_db"]
+db = client["nexus_catch_bot"] # Database နာမည်
 
-users_col = db["users"]          
-cards_col = db["cards"]          
-inventory_col = db["inventory"]  
-market_col = db["marketplace"]
+# Collection များ
+users_col = db["users"]
+cards_col = db["cards"]
+inventory_col = db["inventory"]
