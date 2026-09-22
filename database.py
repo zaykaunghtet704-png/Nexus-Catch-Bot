23c5f34d0920abd6018206bbb641c8bd083801ad
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-09-22 20:20:03 +0630

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
 import os
 from pymongo import MongoClient
 
-# MongoDB လိပ်စာကို ဤနေရာတွင် တိုက်ရိုက်ထည့်ပါ (အပေါ်က Username/Password ထည့်ထားပြီးသား Link အပြည့်အစုံ)
+# MongoDB လိပ်စာကို ဤနေရာတွင် တိုက်ရိုက်ထည့်ပါ
 MONGO_URI = "mongodb+srv://3Maybe:Nsnenfnddn283_283@cluster0.vdys92x.mongodb.net/?appName=Cluster0"
-
-if not MONGO_URI:
-    raise ValueError("MONGO_URI ကို ထည့်သွင်းရန် လိုအပ်ပါသည်။")
 
 # MongoDB client ချိတ်ဆက်ခြင်း
 client = MongoClient(MONGO_URI)
-db = client["nexus_catch_bot"] # Database နာမည်
+db = client["nexus_catch_bot"]
 
 # Collection များ
 users_col = db["users"]
