3e3dcb1fc63da9adeecb43cb3994998956fd4a1e
zaykaunghtet704-png <zaykaunghtet704@gmail.com>
2026-09-23 19:38:30 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+OWNER_ID = 7974865879
+OWNER_USERNAME = "May_Be_786"
+GROUP_LINK = "https://t.me/+00J7JktW8bJlZTY1"
+CHANNEL_LINK = "https://t.me/+E6BxfAj0gaI2Y2Zl"
+
+# 13 Rarity Tiers and Drop Probabilities (Percentages)
+RARITIES = [
+    "Common", "Uncommon", "Rare", "Epic", "Legendary", 
+    "Mythic", "Divine", "Celestial", "Eternal", "Ultimate", 
+    "Supreme", "Transcendent", "Premium Edition"
+]
+
+MAX_SELL_PRICE = 15000
